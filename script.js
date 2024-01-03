62196587d0c27c2b57d9adaf3b1b4a104e1ce5d3
Ibrahim Yoonus <Eunus4040@gmail.com>
2024-01-03 09:04:12 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+        $(window).scroll(function() {
+                if (this.scrollY > 20) {
+                        $('.navbar').addClass("sticky");
+                } else {
+                        $('.navbar').removeClass("sticky");
+                }
+
+                if (this.scrollY > 500) {
+                        $('.scroll-up-btn').addClass("show");
+                } else {
+                        $('.scroll-up-btn').removeClass("show");
+                }
+        });
+
+        $('.scroll-up-btn').click(function() {
+                $('html').animate({ scrollTop: 0 });
+         
+                $('html').css("scrollBehavior", "auto");
+        });
+
+        $('.navbar .menu li a').click(function() {
+          
+                $('html').css("scrollBehavior", "smooth");
+        });
+
+        // toggle menu/navbar script
+        $('.menu-btn').click(function() {
+                $('.navbar .menu').toggleClass("active");
+                $('.menu-btn i').toggleClass("active");
+        });
+
+        var typed = new Typed(".typing", {
+                strings: ["Web designer.", "Web Developer.", "Graphic Designer."],
+                typeSpeed: 200,
+                backSpeed: 200,
+                loop: true
+        });
+
+        var typed = new Typed(".typing-2", {
+                strings: ["Web designer.", "Web Developer.", "Graphic Designer."],
+                typeSpeed: 200,
+                backSpeed: 200,
+                loop: true
+        });
+
+       
+        $('.carousel').owlCarousel({
+                margin: 20,
+                loop: true,
+                autoplay: true,
+                autoplayTimeOut: 2000,
+                autoplayHoverPause: true,
+                responsive: {
+                        0: {
+                                items: 1,
+                                nav: false
+                        },
+                        600: {
+                                items: 2,
+                                nav: false
+                        },
+                        1000: {
+                                items: 3,
+                                nav: false
+                        }
+                }
+        });
+});
