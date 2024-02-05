08271e765d8a79f64cecac731ab23fd0ebca6183
Ibrahim Yoonus <Eunus4040@gmail.com>
2024-02-05 07:22:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("section");
 
-    window.addEventListener("scroll", function () {
-        let currentSection = null;
-
-        sections.forEach((section) => {
-            const rect = section.getBoundingClientRect();
-            if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-                currentSection = section;
-            }
-        });
-
-        if (currentSection) {
-            currentSection.scrollIntoView({ behavior: "smooth" });
-        }
-    });
-});
